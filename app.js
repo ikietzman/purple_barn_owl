e7d023dd442f399400c668062bd4d6567fddaf6b
Ian Kietzman <ian@tallgrassschool.com>
2022-05-09 17:09:27 -0500

    Removed unused DB connection code from app.js

--- app.js
+++ app.js
@@ -12,16 +12,6 @@ const { authMiddleware, checkApplicationStatus } = require('./middleware')
 const app = express()
 const port = 3000;
 
-// // Connect to DB
-// (async () => {
-//   try {
-//     await sequelize.authenticate();
-//     console.log('Connection has been established successfully.')
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error)
-//   }
-// })();
-
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(authMiddleware)
